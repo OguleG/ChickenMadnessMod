696a5fea50f57c4b44bb6b86c5a322bd04ad77f5
guciomlodyraper <92449471+guciomlodyraper@users.noreply.github.com>
2022-01-27 19:57:31 +0100

    Update ModItems.java

--- src/main/java/net/chickenmadness/item/ModItems.java
+++ src/main/java/net/chickenmadness/item/ModItems.java
@@ -20,6 +20,7 @@ public class ModItems {
     public static final Item record_cobble = registerItem("record_cobble", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
     public static final Item record_old = registerItem("record_old", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
     public static final Item record_rest = registerItem("record_rest", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item alloy = registerItem("alloy", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registry.ITEM, new Identifier(ChickenMadness.MOD_ID, name), item);
