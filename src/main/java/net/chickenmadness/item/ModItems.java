b69c3ea3411e81bffbae0be0dedf84e0d371a9de
guciomlodyraper <92449471+guciomlodyraper@users.noreply.github.com>
2022-01-27 13:46:04 +0100

    Music Disc Cobble and Old

--- src/main/java/net/chickenmadness/item/ModItems.java
+++ src/main/java/net/chickenmadness/item/ModItems.java
@@ -17,6 +17,8 @@ public class ModItems {
     public static final Item chicken_rapka = registerItem("chicken_rapka", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
     public static final Item golden_chicken_rapka = registerItem("golden_chicken_rapka", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
     public static final Item dolphin_heart = registerItem("dolphin_heart", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item record_cobble = registerItem("record_cobble", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item record_old = registerItem("record_old", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registry.ITEM, new Identifier(ChickenMadness.MOD_ID, name), item);
