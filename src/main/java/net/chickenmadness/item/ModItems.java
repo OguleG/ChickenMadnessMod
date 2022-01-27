cc87333ab9b7a46c2f225135cda5c75688d69e06
AppaTheBison21 <81151256+AppaTheBison21@users.noreply.github.com>
2022-01-27 12:32:57 +0100

    Update ModItems.java

--- src/main/java/net/chickenmadness/item/ModItems.java
+++ src/main/java/net/chickenmadness/item/ModItems.java
@@ -1,7 +1,9 @@
 package net.chickenmadness.item;
 
 import net.chickenmadness.ChickenMadness;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
