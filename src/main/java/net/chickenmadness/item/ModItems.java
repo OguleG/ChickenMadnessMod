07f301972c1e22a264ca3937d352ca34150b44ca
Igor <igorkomosa0@gmail.com>
2022-01-28 14:25:27 +0100

    Update ModItems.java

--- src/main/java/net/chickenmadness/item/ModItems.java
+++ src/main/java/net/chickenmadness/item/ModItems.java
@@ -2,6 +2,8 @@ package net.chickenmadness.item;
 
 import net.chickenmadness.ChickenMadness;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -12,7 +14,7 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
 
 
-    public static final Item emerald_spider_eye = registerItem("emerald_spider_eye", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(1).saturationModifier(0.2f).build()).group(ItemGroup.MISC)));
+    public static final Item emerald_spider_eye = registerItem("emerald_spider_eye", new Item(new FabricItemSettings().food(new FoodComponent.Builder().hunger(1).saturationModifier(0.2f).statusEffect(new StatusEffectInstance(StatusEffects.POISON,20*10), 1).statusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 20*30), 1).build()).group(ItemGroup.MISC)));
     public static final Item lucky_grass = registerItem( "lucky_grass",new Item(new FabricItemSettings().group(ItemGroup.MISC)));
     public static final Item three_leaf_clover = registerItem("three_leaf_clover",new Item(new FabricItemSettings().group(ItemGroup.MISC)));
     public static final Item chicken_rapka = registerItem("chicken_rapka", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
