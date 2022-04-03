22e2c474da9f20c0b7ae9e07cff8c58332d7fb51
AppaTheBison21 <81151256+AppaTheBison21@users.noreply.github.com>
2022-04-03 22:14:40 +0200

    Update LootTableRegister.java

--- src/main/java/net/chickenmadness/registry/LootTableRegister.java
+++ src/main/java/net/chickenmadness/registry/LootTableRegister.java
@@ -7,15 +7,26 @@ import net.minecraft.loot.entry.LootTableEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.util.Identifier;
 
+
 public class LootTableRegister {
         private static final Identifier GRASS_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/grass");
         private static final Identifier MOD_GRASS_LOOT_TABLE_ID = new Identifier(ChickenMadness.MOD_ID, "blocks/grass");
+        private static final Identifier CHICKEN_LOOT_TABLE_ID = new Identifier("minecraft","mobs/chicken" );
+        private static final Identifier MOD_CHICKEN_LOOT_TABLE_ID = new Identifier(ChickenMadness.MOD_ID,"mobs/chicken");
         public static void register() {
                 LootTableLoadingCallback.EVENT.register(((resourceManager, lootManager, id, table, setter) -> {
                         if (GRASS_LOOT_TABLE_ID.equals(id)) {
                                 FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
                                         .rolls(ConstantLootNumberProvider.create(1))
                                         .with(LootTableEntry.builder(MOD_GRASS_LOOT_TABLE_ID));
+                                table.withPool(poolBuilder.build());
+                        }
+                }));
+                LootTableLoadingCallback.EVENT.register(((resourceManager, lootManager, id, table, setter) -> {
+                        if (CHICKEN_LOOT_TABLE_ID.equals(id)) {
+                                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                                        .rolls(ConstantLootNumberProvider.create(1))
+                                        .with(LootTableEntry.builder(MOD_CHICKEN_LOOT_TABLE_ID));
                                 table.withPool(poolBuilder.build());
                         }
                 }));
